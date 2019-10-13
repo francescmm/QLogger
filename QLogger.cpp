8ca691814e57cd0fec1caf2c7997ce846fcdb826
Francesc Martinez <francescmm@gmail.com>
2019-10-13 18:01:12 +0200

    Fixing QStringBuilder construction for plugins

--- QLogger.cpp
+++ QLogger.cpp
@@ -24,168 +24,175 @@
 
 namespace QLogger
 {
-    void QLog_Trace(const QString &module, const QString &message)
-    {
-        QLog_(module, LogLevel::Trace, message);
-    }
+void QLog_Trace(const QString &module, const QString &message)
+{
+   QLog_(module, LogLevel::Trace, message);
+}
 
-    void QLog_Debug(const QString &module, const QString &message)
-    {
-        QLog_(module, LogLevel::Debug, message);
-    }
+void QLog_Debug(const QString &module, const QString &message)
+{
+   QLog_(module, LogLevel::Debug, message);
+}
 
-    void QLog_Info(const QString &module, const QString &message)
-    {
-        QLog_(module, LogLevel::Info, message);
-    }
+void QLog_Info(const QString &module, const QString &message)
+{
+   QLog_(module, LogLevel::Info, message);
+}
 
-    void QLog_Warning(const QString &module, const QString &message)
-    {
-        QLog_(module, LogLevel::Warning, message);
-    }
+void QLog_Warning(const QString &module, const QString &message)
+{
+   QLog_(module, LogLevel::Warning, message);
+}
 
-    void QLog_Error(const QString &module, const QString &message)
-    {
-        QLog_(module, LogLevel::Error, message);
-    }
+void QLog_Error(const QString &module, const QString &message)
+{
+   QLog_(module, LogLevel::Error, message);
+}
 
-    void QLog_Fatal(const QString &module, const QString &message)
-    {
-        QLog_(module, LogLevel::Fatal, message);
-    }
+void QLog_Fatal(const QString &module, const QString &message)
+{
+   QLog_(module, LogLevel::Fatal, message);
+}
 
-    void QLog_(const QString &module, LogLevel level, const QString &message)
-    {
-        const auto manager = QLoggerManager::getInstance();
+void QLog_(const QString &module, LogLevel level, const QString &message)
+{
+   const auto manager = QLoggerManager::getInstance();
 
-        QMutexLocker(&manager->mutex);
+   QMutexLocker(&manager->mutex);
 
-        const auto logWriter = manager->getLogWriter(module);
+   const auto logWriter = manager->getLogWriter(module);
 
-        if (logWriter and logWriter->getLevel() <= level)
-                logWriter->write(module,message, level);
-    }
+   if (logWriter and logWriter->getLevel() <= level)
+      logWriter->write(module, message, level);
+}
 
-    //QLoggerManager
-    QLoggerManager * QLoggerManager::INSTANCE = nullptr;
+// QLoggerManager
+QLoggerManager *QLoggerManager::INSTANCE = nullptr;
 
-    QLoggerManager::QLoggerManager() : QThread(), mutex(QMutex::Recursive)
-    {
-        start();
+QLoggerManager::QLoggerManager()
+   : QThread()
+   , mutex(QMutex::Recursive)
+{
+   start();
 
-        QDir dir(QDir::currentPath());
+   QDir dir(QDir::currentPath());
 
-        if (!dir.exists("logs"))
-            dir.mkdir("logs");
-    }
+   if (!dir.exists("logs"))
+      dir.mkdir("logs");
+}
 
-    QLoggerManager * QLoggerManager::getInstance()
-    {
-        if (!INSTANCE)
-            INSTANCE = new QLoggerManager();
+QLoggerManager *QLoggerManager::getInstance()
+{
+   if (!INSTANCE)
+      INSTANCE = new QLoggerManager();
 
-        return INSTANCE;
-    }
+   return INSTANCE;
+}
 
-    QString QLoggerManager::levelToText(const LogLevel &level)
-    {
-        switch (level)
-        {
-            case LogLevel::Trace:   return "Trace";
-            case LogLevel::Debug:   return "Debug";
-            case LogLevel::Info:    return "Info";
-            case LogLevel::Warning: return "Warning";
-            case LogLevel::Error:   return "Error";
-            case LogLevel::Fatal:   return "Fatal";
-        }
+QString QLoggerManager::levelToText(const LogLevel &level)
+{
+   switch (level)
+   {
+      case LogLevel::Trace:
+         return "Trace";
+      case LogLevel::Debug:
+         return "Debug";
+      case LogLevel::Info:
+         return "Info";
+      case LogLevel::Warning:
+         return "Warning";
+      case LogLevel::Error:
+         return "Error";
+      case LogLevel::Fatal:
+         return "Fatal";
+   }
 
-        return QString();
-    }
+   return QString();
+}
 
-    bool QLoggerManager::addDestination(const QString &fileDest, const QString &module, LogLevel level)
-    {
-        if (!moduleDest.contains(module))
-        {
-            const auto log = new QLoggerWriter(fileDest,level);
-            moduleDest.insert(module, log);
+bool QLoggerManager::addDestination(const QString &fileDest, const QString &module, LogLevel level)
+{
+   if (!moduleDest.contains(module))
+   {
+      const auto log = new QLoggerWriter(fileDest, level);
+      moduleDest.insert(module, log);
 
-            return true;
-        }
+      return true;
+   }
 
-        return false;
-    }
+   return false;
+}
 
-    bool QLoggerManager::addDestination(const QString &fileDest, const QStringList &modules, LogLevel level)
-    {
-        bool allAdded = false;
+bool QLoggerManager::addDestination(const QString &fileDest, const QStringList &modules, LogLevel level)
+{
+   bool allAdded = false;
 
-        for (const auto &module : modules)
-        {
-            if (!moduleDest.contains(module))
-            {
-                const auto log = new QLoggerWriter(fileDest,level);
-                moduleDest.insert(module, log);
-                allAdded = true;
-            }
-        }
+   for (const auto &module : modules)
+   {
+      if (!moduleDest.contains(module))
+      {
+         const auto log = new QLoggerWriter(fileDest, level);
+         moduleDest.insert(module, log);
+         allAdded = true;
+      }
+   }
 
-        return allAdded;
-    }
+   return allAdded;
+}
 
-    void QLoggerManager::closeLogger()
-    {
-       deleteLater();
-       exit(0);
-    }
+void QLoggerManager::closeLogger()
+{
+   deleteLater();
+   exit(0);
+}
 
-    QLoggerWriter::QLoggerWriter(const QString &fileDestination, LogLevel level)
-    {
-        mFileDestination = "logs/" + fileDestination;
-        m_level = level;
-    }
+QLoggerWriter::QLoggerWriter(const QString &fileDestination, LogLevel level)
+{
+   mFileDestination = "logs/" + fileDestination;
+   m_level = level;
+}
 
-    QString QLoggerWriter::renameFileIfFull()
-    {
-        const auto MAX_SIZE = 1024 * 1024;
-        const auto toRemove = mFileDestination.section('.',-1);
-        const auto fileNameAux = mFileDestination.left(mFileDestination.size() - toRemove.size()-1);
-        auto renamed = false;
-        auto newName = fileNameAux + "_%1__%2.log";
+QString QLoggerWriter::renameFileIfFull()
+{
+   const auto MAX_SIZE = 1024 * 1024;
+   const auto toRemove = mFileDestination.section('.', -1);
+   const auto fileNameAux = mFileDestination.left(mFileDestination.size() - toRemove.size() - 1);
+   auto renamed = false;
+   auto newName = QString("%1%2").arg(fileNameAux, "_%1__%2.log");
 
-        QFile file(mFileDestination);
+   QFile file(mFileDestination);
 
-        //Renaming file if it's full
-        if (file.size() >= MAX_SIZE)
-        {
-            //Creem un fixer nou
-            const auto currentTime = QDateTime::currentDateTime();
-            newName = newName.arg(currentTime.date().toString("dd_MM_yy")).arg(currentTime.time().toString("hh_mm_ss"));
-            renamed = file.rename(mFileDestination, newName);
+   // Renaming file if it's full
+   if (file.size() >= MAX_SIZE)
+   {
+      // Creem un fixer nou
+      const auto currentTime = QDateTime::currentDateTime();
+      newName = newName.arg(currentTime.date().toString("dd_MM_yy"), currentTime.time().toString("hh_mm_ss"));
+      renamed = file.rename(mFileDestination, newName);
+   }
 
-        }
+   return renamed ? newName : QString();
+}
 
-        return renamed ? newName : "";
-    }
+void QLoggerWriter::write(const QString &module, const QString &message, const LogLevel &messageLogLevel)
+{
+   QFile file(mFileDestination);
 
-    void QLoggerWriter::write(const QString &module, const QString &message, const LogLevel &messageLogLevel)
-    {
-        QFile file(mFileDestination);
+   const auto newName = renameFileIfFull();
 
-        const auto newName = renameFileIfFull ();
+   if (file.open(QIODevice::ReadWrite | QIODevice::Text | QIODevice::Append))
+   {
+      QTextStream out(&file);
+      const auto dtFormat = QDateTime::currentDateTime().toString("dd-MM-yyyy hh:mm:ss.zzz");
 
-        if (file.open(QIODevice::ReadWrite | QIODevice::Text | QIODevice::Append))
-        {
-            QTextStream out(&file);
-            const auto dtFormat = QDateTime::currentDateTime().toString("dd-MM-yyyy hh:mm:ss.zzz");
+      if (!newName.isEmpty())
+         out << QString("%1 - Previous log %2\n").arg(dtFormat).arg(newName);
 
-            if (!newName.isEmpty())
-                out << QString("%1 - Previous log %2\n").arg(dtFormat).arg(newName);
+      const auto logLevel = QLoggerManager::levelToText(messageLogLevel);
+      const auto text = QString("[%1] [%2] {%3} %4\n").arg(dtFormat).arg(logLevel).arg(module).arg(message);
 
-            const auto logLevel = QLoggerManager::levelToText(messageLogLevel);
-            const auto text = QString("[%1] [%2] {%3} %4\n").arg(dtFormat).arg(logLevel).arg(module).arg(message);
-
-            out << text;
-            file.close();
-        }
-    }
+      out << text;
+      file.close();
+   }
+}
 }
